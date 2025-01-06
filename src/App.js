c0fa43c6351cf19bcf69bbcda9e995e24a2766f8
Your Name <your.email@example.com>
2025-01-06 12:18:19 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import ArweaveWallet from './components/ArweaveWallet';
-import { HashRouter } from 'react-router-dom';  // Mengimpor HashRouter
+import { HashRouter } from 'react-router-dom'; // Mengimpor HashRouter
 
 function App() {
   return (
